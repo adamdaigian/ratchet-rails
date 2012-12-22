40bdd4fb06cf7234cb0ce9d6548a687435720ce6
Christopher Hopkins <hopkins.chris@gmail.com>
2012-12-22 14:20:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RatchetRails::Application.config.secret_token = '1e97f23bc1c25239a7c1a6b6c35a0a6d24ce79dccd189c4ac7fdf22d686eb6ff4ac5d4fe0b832ceb365bd092d3c437a13a827ba5ff0d9892a9a276cfd258abe3'
